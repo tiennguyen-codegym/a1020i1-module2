be1774e2c6616864282dc059eff4b2a55004e64d
pato <tien.nguyen@codegym.vn>
2021-03-15 19:14:20 +0700

    tien.nguyen add string regex

---
+++ a10i-java/src/_18_string_regex/TestRegex.java
@@ -0,0 +1,25 @@
+package _18_string_regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TestRegex {
+
+    public static void main(String[] args) {
+
+        // Regex, Quantifier
+//        String strInput = "ac";
+//        System.out.println(strInput.matches("[abc]?"));
+
+        // Kiem tra dinh dang email
+//        String emailInput = "234abc_3432@codegym.com";
+//        System.out.println(emailInput.matches("[A-Za-z_0-9]+@(gmail|codegym).com"));
+//        System.out.println(emailInput.matches("[\\w]+@(gmail|codegym).com"));
+
+        String emailInput = "234abc_3432@codegym.com";
+        Pattern pattern = Pattern.compile("[\\w]+@(gmail|codegym).com");
+        Matcher matcher = pattern.matcher(emailInput);
+
+        System.out.println(matcher.matches());
+    }
+}
---
+++ a10i-java/src/_18_string_regex/TestString.java
@@ -0,0 +1,32 @@
+package _18_string_regex;
+
+public class TestString {
+
+    public static void main(String[] args) {
+
+        // String methods
+//        String str = "   a1020i1         ";
+//        System.out.println(str.charAt(3));
+//        System.out.println(str.concat("hello"));
+//        System.out.println(str);
+//        System.out.println(str.trim());
+//
+//        System.out.println("a1020i1".equals("A1020i1"));
+//        System.out.println("a1020i1".equalsIgnoreCase("A1020i1"));
+//
+//        System.out.println("abc".compareTo("abd"));
+//
+//        System.out.println("abc".startsWith("ba"));
+//
+//        System.out.println("nhan".contains("N"));
+
+        // StringBuilder
+        StringBuilder stringBuilder = new StringBuilder("Hello ");
+        stringBuilder.append("World");
+
+        String strTmp = stringBuilder.toString();
+        System.out.println(strTmp);
+
+//        StringBuilder stringBuilder1 = new StringBuilder(strTmp);
+    }
+}
